fbea32d5713cc08df06229d49d9f90293c48d8bf
ColorizeMySky <colorizemysky@gmail.com>
2019-02-03 16:48:32 +0300

    add Listener part 1

--- inbreeding-coefficients.js
+++ inbreeding-coefficients.js
@@ -1,4 +1,34 @@
-const colors = ["#004D40", "#006064", "#0D47A1", "#1A237E", "#1B5E20", "#3E2723", "#4A148C",
-				"#4DB6AC", "#4DD0E1", "#64B5F6", "#7986CB", "#81C784", "#827717", "#880E4F",
-				"#A1887F", "#B71C1C", "#BA68C8", "#DCE775", "#E57373", "#E65100", "#F06292", "#FFB74D" ];
-const ancestorQuantity = 0;
+const table = document.querySelector('.pedigreetable');
+
+const colors = ['azure', 'beige', 'coral', 'darkseagreen', 'mediumorchid', 'silver', 'teal'];
+let ancestorQuantity = 0;
+
+table.addEventListener('click', function(event){
+  event.target.classList.toggle("selected-td");
+  event.target.classList.toggle(colors[ancestorQuantity]);
+  event.target.style.backgroundColor = colors[ancestorQuantity];
+
+  if(event.target.classList.contains('selected-td')) {
+    if(event.target.classList.contains('f1') || event.target.classList.contains('f2') 
+       || event.target.classList.contains('f3') || event.target.classList.contains('f4')) {
+
+        let inactive = document.querySelectorAll('.m1, .m2, .m3, .m4');
+        for (let item of inactive) {
+          item.classList.add("inactive");
+        }
+    }
+    else {
+      let inactive = document.querySelectorAll('.f1, .f2, .f3, .f4');
+      for (let item of inactive) {
+        item.classList.add("inactive");
+      }
+    }
+  }
+})
+
+function addAncestor() {
+  if (ancestorQuantity > colors.length) {
+    document.querySelector('#pedigree-script-result')
+      .innerHTML = "Количество предков более " + colors.length + " не поддерживается в настоящее время";
+  }
+}
